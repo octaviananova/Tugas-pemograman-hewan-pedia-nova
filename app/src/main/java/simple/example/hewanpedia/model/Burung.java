5e4ea25f21d583ef113c8d28d19fc17088e66880
octaviananova <octaviananova729@gmail.com>
2020-11-23 22:38:13 +0700

    Tugaspemogramanmobile

---
+++ app/src/main/java/simple/example/hewanpedia/model/Burung.java
@@ -0,0 +1,10 @@
+package simple.example.hewanpedia.model;
+
+public class Burung extends Hewan{
+
+    public Burung(String ras, String asal, String deskripsi, int drawableRes) {
+        super("Burung",ras,asal,deskripsi,drawableRes);
+
+    }
+
+}
